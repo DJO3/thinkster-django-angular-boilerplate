94db4d1eb9ead7e5ec8d807619dc0adc5d553be1
Dave <oconnorwork@gmail.com>
2014-12-27 13:17:32 -0500

    Logout Success

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,14 @@
-from rest_framework import permissions, viewsets, status
+import json
+
+from django.contrib.auth import authenticate, login, logout
+
+from rest_framework import permissions, viewsets, status, views
 from rest_framework.response import Response
 
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
@@ -19,14 +24,52 @@ class AccountViewSet(viewsets.ModelViewSet):
 
         return (permissions.IsAuthenticated(), IsAccountOwner(),)
 
-def create(self, request):
-    serializer = self.serializer_class(data=request.data)
+    def create(self, request):
+        serializer = self.serializer_class(data=request.DATA)
 
-    if serializer.is_valid():
-        Account.objects.create_user(**serializer.validated_data)
-        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            account = Account.objects.create_user(**request.DATA)
 
-    return Response({
-    'status': 'Bad request',
-    'message': 'Account could not be created with received data.'
-    }, status=status.HTTP_400_BAD_REQUEST)
+            account.set_password(request.DATA.get('password'))
+            account.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
